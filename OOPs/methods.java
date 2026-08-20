0b8ff8492aaee3191641605562d160a279c3225f
antimap861-jpg <antimap861@gmail.com>
2026-08-21 03:42:08 +0530

    feat: implement method overloading in Calculator class and update main method usage

--- OOPs/methods.java
+++ OOPs/methods.java
@@ -1,10 +1,27 @@
-public class Methods {
-
-    static void greet() {
-        System.out.println("Hello from method!");
+// Method overloading
+class Calculator {
+    public int add(int n1, int n2) {
+        return n1 + n2;
     }
 
-    public static void main(String[] args) {
-        greet();
+    // Same method with diffrent parameters
+    public int add(int n1, int n2, int n3) {
+        return n1 + n2 + n3;
+    }
+
+    // Diffrent type of parmter with same method
+    public double add(double n1, double n2) {
+        return n1 + n2;
+    }
+
+}
+
+    public classMethods{
+
+    public static void main(String[] args)
+      {
+        Calculator calc = new Calculator();
+        int result = calc.add(10, 20);
+        System.out.println(result);
     }
 }
